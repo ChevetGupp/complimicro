da395d46df95d27f8893d4aac35db9776a75c1e3
ChevetGupp <carloshenriquearaujo1212@gmail.com>
2023-09-28 17:06:40 -0300

    Adicinado para criar script

---
+++ teste.ps1
@@ -0,0 +1,23 @@
+﻿# Obter o caminho da pasta atual do script
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Definir o caminho da pasta de instalação dos programas
+$folderPath = Join-Path $scriptPath "apps/teste"
+
+# Caminho para o instalador do aplicativo
+$instalador = Join-Path $folderPath "MediaCreationTool22H2.exe"
+
+# Verifique se o instalador existe
+if (Test-Path $instalador) {
+    # Execute o instalador silenciosamente
+    Start-Process -FilePath $instalador -ArgumentList "/S" -Wait
+
+    # Verifique se a instalação foi bem-sucedida
+    if ($?) {
+        Write-Host "Instalação concluída com sucesso."
+    } else {
+        Write-Host "A instalação falhou."
+    }
+} else {
+    Write-Host "O instalador não foi encontrado em $instalador."
+}
